d9da8a01e479b2c0338b0388ecd56df807ff0336
Arbaz Siddiqui <arbaz00@gmail.com>
2017-05-17 20:56:16 +0530

    render and embed home video

--- app/controllers/playCtrl.js
+++ app/controllers/playCtrl.js
@@ -1,4 +1,10 @@
 angular.module('playCtrl', [])
-  .controller('PlayController', function ($scope, $rootScope) {
-    $scope.test = "This is a test messege to check connections."
+  .controller('PlayController', function ($scope, $rootScope, Play,$sce) {
+    Play.getVideos()
+      .success(function (data) {
+        var videos = data.data.children;
+        var div = document.createElement('div');
+        div.innerHTML = videos[0].data.media.oembed.html;
+        $scope.url =  $sce.trustAsHtml(div.textContent);
+      });
   });
--- app/services/playService.js
+++ app/services/playService.js
@@ -1,4 +1,10 @@
 angular.module('playService', [])
   .factory('Play', function ($http) {
-    
+    var playFactory = {};
+
+    playFactory.getVideos = function () {
+      return $http.get('https://api.reddit.com/r/videos/hot.json');
+    };
+
+    return playFactory;
   });
